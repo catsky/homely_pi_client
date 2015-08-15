0a423ad0da7388999ac4bd0ed379de8eb4026b70
Ken Zheng <zhdhui@gmail.com>
2015-08-15 21:04:46 +1000

    more details

--- cameras/views.py
+++ cameras/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+from .models import Image
+
+def index(request):
+  latest_image_list = Image.objects.order_by('-created_at')[:5]
+  context = {
+     'latest_image_list': latest_image_list,
+  }
+  return render(request, 'cameras/index.html', context)
+
+def detail(request, image_id):
+  image = get_object_or_404(Image, pk=image_id)
+  return render(request, 'cameras/detail.html', {'image': image})
