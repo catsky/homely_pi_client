c2fb1e19e8cb8e4002b585b17146565b6c191a6e
Ken Zheng <zhdhui@gmail.com>
2015-09-07 21:03:22 +1000

    mysql

--- homely_pi_client/settings.py
+++ homely_pi_client/settings.py
@@ -90,9 +90,13 @@ DEFAULT_TO_EMAIL = email_settings['DEFAULT_TO_EMAIL']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'homelypi',
+            'USER': 'root',
+            'PASSWORD': 'iamglad2passw0rd',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        }
 }
 
 
