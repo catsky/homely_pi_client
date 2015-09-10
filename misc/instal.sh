080bec08300a5d9fc9cf88f7d88f9e149fe7c425
Ken Zheng <zhdhui@gmail.com>
2015-09-10 19:59:01 +1000

    postgresql update

--- misc/instal.sh
+++ misc/instal.sh
@@ -3,9 +3,17 @@ parent=`dirname "$script_abspath"`
 RELEASE_PATH=`dirname "$parent"`
 
 
+sudo apt-get update
+
 echo ">>> Install software..."
 sudo apt-get install libpq-dev python-dev
 
-echo ">>> Install Mysql Server >>>"
-bash $RELEASE_PATH/misc/auto_mysql.sh
+echo ">>> Install Postgresql... >>>"
+sudo apt-get install postgresql postgresql-contrib
+# sudo su - postgres
+# createdb homelypi
+# createuser -P #root
+# psql
+# GRANT ALL PRIVILEGES ON DATABASE homelypi TO root;
 
+python manage.py syncdb
